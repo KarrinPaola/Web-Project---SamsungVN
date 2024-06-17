48ba6c52408e2c51f4119210c5da89914db0b733
KarinPaola <tryingbidulee@gmail.com>
2024-06-17 10:49:45 +0700

    slider

--- MainPage/Code/main.js
+++ MainPage/Code/main.js
@@ -1,11 +1,67 @@
-// Lấy thẻ a cần xử lý sự kiện click
-const menuToggle = document.querySelector('#header .nav-bar div a');
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let playPause = document.getElementById('play-pause');
 
-// Lấy phần nav-bar cần thay đổi
-const navBar = document.querySelector('#header .nav-bar');
+let active = 0;
+let lengthItems = items.length;
+let isPlaying = true;
 
-// Thêm sự kiện click vào thẻ a
-menuToggle.addEventListener('click', function() {
-    // Chuyển thuộc tính right của nav-bar sang 0 khi click
-    navBar.style.right = '-100%';
+function nextSlide() {
+    if (active + 1 >= lengthItems) {
+        active = 0;
+    } else {
+        active += 1;
+    }
+    reloadSlider();
+}
+
+next.onclick = nextSlide;
+
+prev.onclick = function() {
+    if (active - 1 < 0) {
+        active = lengthItems - 1;
+    } else {
+        active -= 1;
+    }
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(nextSlide, 5000);
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    if (lastActiveDot) {
+        lastActiveDot.classList.remove('active');
+    }
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    if (isPlaying) {
+        refreshSlider = setInterval(nextSlide, 5000);
+    }
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function() {
+        active = key;
+        reloadSlider();
+    });
+});
+
+playPause.addEventListener('click', function() {
+    isPlaying = !isPlaying;
+    if (isPlaying) {
+        refreshSlider = setInterval(nextSlide, 5000);
+        playPause.querySelector('.play').style.display = 'none';
+        playPause.querySelector('.pause').style.display = 'inline';
+    } else {
+        clearInterval(refreshSlider);
+        playPause.querySelector('.play').style.display = 'inline';
+        playPause.querySelector('.pause').style.display = 'none';
+    }
 });
