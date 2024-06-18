a370a7e9e3287c207f02fc879058e1891a65b475
KarinPaola <tryingbidulee@gmail.com>
2024-06-19 06:06:49 +0700

    slider

--- MainPage/Code/main.js
+++ MainPage/Code/main.js
@@ -65,3 +65,99 @@ playPause.addEventListener('click', function() {
         playPause.querySelector('.pause').style.display = 'none';
     }
 });
+
+let showcase = document.querySelector('#content .sale-hot .showcase');
+let cases = document.querySelectorAll('#content .sale-hot .showcase .case');
+let buttons = document.querySelectorAll('#content .sale-hot>ul>li>button')
+
+let active2 = 0; 
+let lengthCase = cases.length; 
+
+function reloadSlider2() {
+    let checkLeft = cases[active2].offsetLeft; 
+    console.log(checkLeft);
+    showcase.style.left = -checkLeft + 'px';
+
+    let lastActiveButton = document.querySelector('#content .sale-hot>ul>li>.active')
+    if(lastActiveButton){
+        lastActiveButton.classList.remove('active'); 
+    }
+    buttons[active2].classList.add('active'); 
+}
+buttons.forEach((button, key) => {
+    button.addEventListener('click', function(){
+        active2 = key; 
+        reloadSlider2(); 
+    })
+})
+document.addEventListener('DOMContentLoaded', function() {
+    var items = document.querySelectorAll('.item');
+
+    items.forEach(function(item) {
+        item.addEventListener('mouseenter', function() {
+            var buyNow = item.querySelector('.buy-now');
+            buyNow.style.visibility = 'visible';
+            buyNow.style.opacity = '1';
+        });
+
+        item.addEventListener('mouseleave', function() {
+            var buyNow = item.querySelector('.buy-now');
+            buyNow.style.visibility = 'hidden';
+            buyNow.style.opacity = '0';
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const cards = document.querySelectorAll('.show-limit-pr a');
+
+    cards.forEach(card => {
+        card.addEventListener('mouseenter', () => {
+            const cta = card.querySelector('.card-cta');
+            cta.style.opacity = '1';
+            cta.style.visibility = 'visible';
+        });
+
+        card.addEventListener('mouseleave', () => {
+            const cta = card.querySelector('.card-cta');
+            cta.style.opacity = '0';
+            cta.style.visibility = 'hidden';
+        });
+    });
+});
+
+let product = document.querySelectorAll('.show-limit-pr a')
+let list_pr = document.querySelector('.list-limited-product')
+let limit_pr = document.querySelector('.show-limit-pr');
+let prev_limit = document.querySelector('.list-limited-product .next-prev .prev');
+let next_limit = document.querySelector('.list-limited-product .next-prev .next');
+
+let active3 = 4; 
+let current = 0; 
+let lengthLimit = product.length; //6
+let size_item = product[0].offsetWidth; 
+console.log(size_item)
+
+next_limit.onclick = function (){
+    console.log('hello')
+    if(active3 + 1 > lengthLimit){
+        next_limit.style.visibility = 'hidden';
+        prev_limit.style.visibility = 'visible';
+    }else{
+        active3 += 1; 
+        current -= size_item; 
+        limit_pr.style.left = current + 'px'; 
+    }
+    console.log(limit_pr.style.left)
+}
+
+prev_limit.onclick = function(){
+    if(active3 - 1 < 4){
+        next_limit.style.visibility = 'visible';
+        prev_limit.style.visibility = 'hidden';
+    }else{
+        active3 -= 1; 
+        current += size_item; 
+        limit_pr.style.left = current + 'px'; 
+    }
+} 
+updateLimit(); 
